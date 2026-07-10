10bd90f5564277b5994bd170a5c755bee52b752e
Khairul Anwar <rayvin201101@gmail.com>
2026-07-10 22:58:10 +0800

    update preloader branding to Ruang Cerdas

--- resources/views/partials/preloader.blade.php
+++ resources/views/partials/preloader.blade.php
@@ -1,11 +1,16 @@
 <div class="preloader" id="preloader">
-    <div class="preloader">
-        <div class="waviy position-relative">
-            <span class="d-inline-block">T</span>
-            <span class="d-inline-block">R</span>
-            <span class="d-inline-block">E</span>
-            <span class="d-inline-block">Z</span>
-            <span class="d-inline-block">O</span>
-        </div>
+    <div class="waviy position-relative">
+        <span class="d-inline-block">R</span>
+        <span class="d-inline-block">U</span>
+        <span class="d-inline-block">A</span>
+        <span class="d-inline-block">N</span>
+        <span class="d-inline-block">G</span>
+
+        <span class="d-inline-block ms-2">C</span>
+        <span class="d-inline-block">E</span>
+        <span class="d-inline-block">R</span>
+        <span class="d-inline-block">D</span>
+        <span class="d-inline-block">A</span>
+        <span class="d-inline-block">S</span>
     </div>
 </div>
